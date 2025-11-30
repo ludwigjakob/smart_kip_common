be88bf0a0b6a7ad597a2decc199f660564a61b76
Ludwig Jakob <ludwig.jakob91@googlemail.com>
2025-11-30 05:17:37 +0000

    integration influxwriter in temperature_connector

--- data_connector/temperature_connector.py
+++ data_connector/temperature_connector.py
@@ -1,4 +1,5 @@
-from influxdb_client import InfluxDBClient
+from influxdb_client import InfluxDBClient, Point, WritePrecision
+from influxdb_client.client.write_api import SYNCHRONOUS
 from common.data_connector.base_connector import BaseConnector
 import os
 from dotenv import load_dotenv
@@ -14,6 +15,7 @@ class TemperatureConnector(BaseConnector):
         influx_url = os.getenv("INFLUX_URL")
         influx_token = os.getenv("INFLUX_TOKEN")
         influx_org = os.getenv("INFLUX_ORG")
+        self.org = influx_org
 
         # Optional: Debug-Ausgabe zur Kontrolle
         if not all([influx_url, influx_token, influx_org]):
@@ -24,6 +26,8 @@ class TemperatureConnector(BaseConnector):
             token=influx_token,
             org=influx_org
         )
+        self.write_api = self.client.write_api(write_options=SYNCHRONOUS)
+
 
     def read(self):
         query = f'''
@@ -38,3 +42,11 @@ class TemperatureConnector(BaseConnector):
             for record in table.records:
                 return round(record.get_value(), 2)
         return None
+
+    def write(self, data: dict):
+        point = (
+            Point(data["type"])
+            .tag("sensor", data["sensor"])
+            .field("value", data["value"])
+        )
+        self.write_api.write(bucket=self.bucket, org=self.org, record=point)
